5bdc70808c7e4005c9671f469774934efd9cfac1
Pedro Ubiratan <pedrohsus853@gmail.com>
2026-01-19 16:08:01 -0300

    feat: primeiro programa em C#

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Qual o seu nome?");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Qual a sua idade?");
+int idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Olá, {nome}. Você tem {idade} anos.");
